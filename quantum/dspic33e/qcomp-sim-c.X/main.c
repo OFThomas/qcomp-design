0e6e66ffdb955e1bf6a3c9e73c4679212d3bd5e5
John Scott <johnrscott0@gmail.com>
2018-10-10 15:50:56 +0100

    Added and tested some simple C IO code

--- quantum/dspic33e/qcomp-sim-c.X/main.c
+++ quantum/dspic33e/qcomp-sim-c.X/main.c
@@ -3,11 +3,49 @@
  *
  */
 
-
+#include "p33EP512MU810.h"
 #include "xc.h"
 
+#define red 0
+#define amber 1
+#define green 2
+
+#define off 0
+#define on 1
+
+#define sw1 6
+#define sw2 7
+#define sw3 13
+
+int set_led(int color, int state) {
+    if (state == on) 
+        LATD |= (1 << color);
+    else 
+        LATD &= ~(1 << color);
+    return 0;
+}
+
+int read_sw(int sw) {
+    if ((sw != sw1) && (sw != sw2) && (sw != sw3)) {
+        return -1;
+    } else {
+        // How well do you know C:
+        return (((PORTD & (1 << sw)) >> sw) ^ 0x0001);
+    }
+}
+
 int main(void) {
-    while (1 == 1)
-        ;
+    
+    // Set up the input/output
+    ANSELD = 0x0000; // Set port D as digital
+    TRISD = 0x20C0; // Set liens 0,1,2 as output; 6,7,13 as input 
+    
+    while (1 == 1) {
+        set_led(red, read_sw(sw1));
+        set_led(amber, read_sw(sw2));
+        set_led(green, read_sw(sw3));
+        
+        
+    }
     return 0;
 }
