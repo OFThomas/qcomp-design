f39340cde45d5bfe52307156953cf0a8a1bbedde
John Scott <johnrscott0@gmail.com>
2018-11-06 20:51:14 +0000

    Added some pin mappings

--- quantum/dspic33e/qcomp-sim-c.X/spi.c
+++ quantum/dspic33e/qcomp-sim-c.X/spi.c
@@ -12,10 +12,15 @@
 int setup_spi(void) {
     
     // Setup the SPI pins
-    // J10:41 = J1:91 = uC:70
-    // J10:44 = J1:93 = uC:9
-    // J10:43 = J1:95 = uC:72
-    // J10:46 = J1:97
+    // J10:41 = J1:91 = uC:70 = RPI74 (PPS code: 0100 1010)
+    // J10:44 = J1:93 = uC:9 = RPI52 (PPS code: 011 0100)
+    // J10:47 = J1:101 = uC:34 = RPI42 (PPS code: )
+    // J10:43 = J1:95 = uC:72 = RP64
+    // J10:46 = J1:97 = uC:69 = RPI73
+    // J10:7 = J1:13 = uC:3 = RP85
+    // J10:5 = J1:7 = uC:5 = RP87
+    // J10:55 = J1:117 = uC:10 = RP118
+    
     RPOR1 
     
     // Configure the SPI registers
