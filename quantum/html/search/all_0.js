3e0f62b5e8f8b1461a6757c4e6cf411786c8d8ce
Oliver <oliver.thomas404@gmail.com>
2018-11-07 11:11:32 +0000

    set EXTRACT_ALL = NO in Doxfile

--- quantum/html/search/all_0.js
+++ quantum/html/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['_5f_5fattribute_5f_5f',['__attribute__',['../io_8c.html#a64653c452ab199760e9cdd6bdbbeeb76',1,'io.c']]]
+  ['led_5fglobal',['LED_GLOBAL',['../structLED__GLOBAL.html',1,'']]]
 ];
--- quantum/html/search/searchdata.js
+++ quantum/html/search/searchdata.js
@@ -1,39 +1,18 @@
 var indexSectionsWithContent =
 {
-  0: "_acdfgilmopqrstvz",
-  1: "l",
-  2: "acimpqst",
-  3: "_acdfilmorst",
-  4: "ls",
-  5: "cmqv",
-  6: "s",
-  7: "imopz",
-  8: "agloprs"
+  0: "l",
+  1: "l"
 };
 
 var indexSectionNames =
 {
   0: "all",
-  1: "classes",
-  2: "files",
-  3: "functions",
-  4: "variables",
-  5: "typedefs",
-  6: "enums",
-  7: "enumvalues",
-  8: "defines"
+  1: "classes"
 };
 
 var indexSectionLabels =
 {
   0: "All",
-  1: "Data Structures",
-  2: "Files",
-  3: "Functions",
-  4: "Variables",
-  5: "Typedefs",
-  6: "Enumerations",
-  7: "Enumerator",
-  8: "Macros"
+  1: "Data Structures"
 };
 
