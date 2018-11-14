e8c0eb7bb7f3865fd2e40515b59e40feaf5aaf09
John Scott <johnrscott0@gmail.com>
2018-11-14 19:03:11 +0000

    Deleted some commented lines

--- quantum/dspic33e/qcomp-sim-c.X/quantum.c
+++ quantum/dspic33e/qcomp-sim-c.X/quantum.c
@@ -117,44 +117,40 @@ void qubit_display(Complex state[], int N) {
  */
 void single_qubit_op(Complex state[], int k, int N, Complex op[2][2]) {
 
-//Q15 temp1 = 0;
-//Q15 temp2 = 0;
+    //Q15 temp1 = 0;
+    //Q15 temp2 = 0;
 
-// do row 1 of op onto all pairs of state vectors
-// e.g. qubit 0, pairs of values are
-// 000, 001
-// then
-// 010, 011
-// etc.
-//
-// op   ( a b ) * ( 000 ) = (temp1)
-//      ( c d )   ( 001 )   (temp2)
-// temp1 = a(000) + b(001) -> new (000) val
-// temp2 = c(000) + d(001) -> new (001) val
+    // do row 1 of op onto all pairs of state vectors
+    // e.g. qubit 0, pairs of values are
+    // 000, 001
+    // then
+    // 010, 011
+    // etc.
+    //
+    // op   ( a b ) * ( 000 ) = (temp1)
+    //      ( c d )   ( 001 )   (temp2)
+    // temp1 = a(000) + b(001) -> new (000) val
+    // temp2 = c(000) + d(001) -> new (001) val
 
-int n_max;
-int j_max;
-int index;
+    int n_max;
+    int j_max;
+    int index;
 
-// qubit zero entries are adjacent 2^0
-// qubit 1 entries are 2^1 apart etc...
+    // qubit zero entries are adjacent 2^0
+    // qubit 1 entries are 2^1 apart etc...
 
-/// loop over n, 2^(current qubit)
-n_max = pow(2,k);
+    /// loop over n, 2^(current qubit)
+    n_max = pow(2, k);
 
     /// Loop here for each contribution to the zero and one amplitude
     for (int n = 0; n < n_max; n++) {
         /// 2^(total qbits - current) 
-        j_max = pow(2, 2-k);
+        j_max = pow(2, 2 - k);
 
         /// loop over j
         for (int j = 0; j < j_max; j++) {
-            /// n + j * 2^(i+1)
-            index = n + (pow(2, k + 1) * j);
-            /// zeros n 
-            //zero_amp += pow(state[index][0],2);
-            /// ones index are always n+1 for zero amps 
-            //one_amp += pow(state[index + n_max][0],2);
+            /// n + j * 2^(k+1)
+            index = n + (j * pow(2, k + 1));
             mat_mul(op, state, index, index + n_max);
         }
     }
